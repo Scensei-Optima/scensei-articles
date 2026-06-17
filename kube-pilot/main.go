160a71d7180c3f071fe971a5ff1dacf4e40a0568
Aleksander Wojnarowicz <aleksander@scensei.com>
2026-06-17 11:43:10 +0200

    semver

--- kube-pilot/main.go
+++ kube-pilot/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/scensei-articles/kubepilot/internal/ui"
 )
 
-const version = "0.1.1"
+const version = "0.2.0"
 
 func main() {
 	localPort := flag.Int("local", 8080, "The local port to bind to")
